95bc1c2d2e848483c073ab5e601ad470d2a92000
yun <yun@yunui-MacBook-Pro.local>
2019-12-23 14:27:19 +0900

    slider prototyping

--- pilots/AnimationGround.playground/Contents.swift
+++ pilots/AnimationGround.playground/Contents.swift
@@ -1,4 +1,59 @@
-// https://www.appcoda.com/interactive-animation-uiviewpropertyanimator/
+import Foundation
+import UIKit
+import XCPlayground
+
+let containerView = UIView(frame: CGRect(x: 0.0, y: 0.0, width: 320.0, height: 640.0))
+XCPlaygroundPage.currentPage.liveView = containerView
+containerView.backgroundColor = UIColor.white
+
+// Drawing the borders
+let sliderFrame = CGRect(x: 100, y: 30, width: 30, height: 200)
+let borderLayer = CALayer()
+borderLayer.frame = sliderFrame
+
+borderLayer.backgroundColor = UIColor.white.cgColor
+borderLayer.borderColor = UIColor(white: 0.9, alpha: 1.0).cgColor
+borderLayer.borderWidth = 1
+borderLayer.cornerRadius = sliderFrame.size.width / 2.0
+borderLayer.masksToBounds = true
+
+containerView.layer.addSublayer(borderLayer)
+
+// Drawing the track
+let trackLayer = CALayer()
+
+trackLayer.anchorPoint = CGPoint(x: 0.5, y: 1)
+trackLayer.backgroundColor = UIColor.red.cgColor
+trackLayer.frame = CGRect(x: 0, y: 0, width: 30, height: 80)
+trackLayer.position = CGPoint(
+    x: borderLayer.bounds.size.width / 2,
+    y: borderLayer.bounds.size.height / 2
+)
+borderLayer.addSublayer(trackLayer)
+
+// Drawing the handle
+let handleLayer = CALayer()
+
+handleLayer.position = borderLayer.position
+handleLayer.bounds.size = CGSize(
+    width: borderLayer.bounds.size.width,
+    height: borderLayer.bounds.size.width
+)
+handleLayer.cornerRadius = borderLayer.bounds.size.width / 2.0
+handleLayer.backgroundColor = UIColor.white.cgColor
+handleLayer.shadowColor = UIColor.black.cgColor
+handleLayer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+handleLayer.shadowRadius = 2
+handleLayer.shadowOpacity = 0.3
+handleLayer.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+
+containerView.layer.addSublayer(handleLayer)
+
+
+
+
+
+/* https://www.appcoda.com/interactive-animation-uiviewpropertyanimator/
 
 import UIKit
 import PlaygroundSupport
@@ -22,5 +77,5 @@ animator.startAnimation()
 PlaygroundPage.current.liveView = containerView
 
 
-
+*/
 
